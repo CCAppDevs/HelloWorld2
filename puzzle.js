19fb9710bc99a23d5557bb696d7d53da08834ceb
Jesse Harlan <gotsanity@gmail.com>
2023-04-06 11:54:19 -0700

    uploading for class

---
+++ puzzle.js
@@ -0,0 +1,39 @@
+let a = 5;
+let b = 15;
+
+// math operators
+// + addition
+// - subtraction
+// * multiplication
+// / division
+// () parenthesis
+// ++ increment
+// -- decrement
+// % modulo (remainder of division)
+
+// boolean operators
+// == equals
+// === equals (explicit)
+// != not equals
+// > greater than
+// < less than
+// >= greater than or equal to
+// <= less than or equal to
+
+// logical operators
+// && AND
+// || OR
+// ! NOT
+// ^ XOR (Exclusive OR)
+
+let age = 40;
+let fname = "jesse";
+
+let answer = (age > 50) || (fname == "jesse");
+//let answer = true;
+
+// set answer to the sum of a + b
+// but don't use the + operator
+
+console.log("The answer is: ", answer);
+// console.log("The answer is correct: ", answer == 20);
